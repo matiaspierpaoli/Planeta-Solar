34d547e2da81694cdc0e9cd5bf4ce49d3e10334e
matiaspierpaoli <matiaspierpaoli@gmail.com>
2022-03-17 00:10:02 -0300

    Finalizada primera parte

--- Sistema_Solar_TP1_Pierpaoli/Assets/Scripts/Rotacion.cs
+++ Sistema_Solar_TP1_Pierpaoli/Assets/Scripts/Rotacion.cs
@@ -7,6 +7,7 @@ public class Rotacion : MonoBehaviour
     public Transform target;
     public int speed;
 
+    // Update is called once per frame
     void Update()
     {
         transform.RotateAround(target.transform.position, target.transform.up, speed * Time.deltaTime);
